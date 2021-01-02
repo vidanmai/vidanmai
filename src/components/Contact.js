74b332351068be3b984f4b7dfbd74076ae8c16eb
vidanmai <vidanmai@gmail.com>
2021-01-02 22:49:08 +0100

    changes to Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,7 @@ class Contact extends React.Component {
     render() {
         return (
             <div className="case">
+                
                 <h1>CONTACT</h1>
                 <h1>CONTACT</h1>
                 <h1>CONTACT</h1>
