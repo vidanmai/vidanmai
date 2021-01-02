f0beb8b5a7f13d0e9d176b787d41f61a36c257b9
vidanmai <vidanmai@gmail.com>
2021-01-02 22:48:26 +0100

    Added few stylistic changes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './style/home.css';
 
 class HomePage extends React.Component {
     render() {
         return (
-            <div className="case">
+            <div className="case" id="home">
                 <h1>VIDANMAI</h1>
+                <div className="grid">
+                    <div id="left">
+                        <div className="center">
+                            <h2>Portfolio</h2>
+                        </div>
+                    </div>
+                    <div id="right">
+                        <h2>Projects</h2>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../style/nav.css';
 
 class Nav extends React.Component {
     render() {
         return (
-            <div>
-                <h1>NAV</h1>
-                <div className="links">
-                    <ul className="listLinks">
-                        <Link to='/'>
-                            <li className="link">Hjem</li>
-                        </Link>
-                        <Link to='/contact'>
-                            <li className="link">Contact</li>
-                        </Link>
-                    </ul>
-                </div>
+          <div>
+            <div className="links">
+              <ul className="listLinks">
+                <Link to="/">
+                  <li className="link">Hjem</li>
+                </Link>
+                <Link to="/contact">
+                  <li className="link">Contact</li>
+                </Link>
+              </ul>
             </div>
+          </div>
         );
     }
 }
