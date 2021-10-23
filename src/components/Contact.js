b27c430e14825aaec7dad33311885d8e338a42e3
vidanmai <vidanmai@gmail.com>
2021-10-23 22:57:59 +0200

    Added links and styling to contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,46 @@
 import React from 'react';
 import './style/contact.css';
+import facebookIcon from './icons/icons8-facebook.svg'
+import githubIcon from './icons/icons8-github.svg'
 
 export default function Contact() {
     return (
         <div className="case">
             <div className="py-10 contactContent">
                 <div className="py-20 bg-green-300 contactGrid">
-                    <div className="contactInfo">
-                        <h1 className="text-4xl font-bold text-center">Contact</h1>
+                    <div className="contactInfo center">
+                        <h1 className="text-4xl font-bold">Get in touch!</h1>
+                        <br />
+                        <ul>
+                            <li>Facebook</li>
+                            <a href="https://www.facebook.com/vidan97">
+                                <img src={facebookIcon} alt="" className="center" />
+                            </a>
+                            <br />
+                            <li>Github</li>
+                            <a href="https://github.com/vidanmai">
+                                <img src={githubIcon} alt="" className="center" />
+                            </a>
+                        </ul>
                     </div>
                     <div className="contactInput">
                         <form name="contact" method="post" data-netlify="true">
-                            <input type="hidden" name="form-name" value="contact"/>
-                            <input type="text" placeholder="Enter your name" name="name"/>
-                            <input type="email" placeholder="Enter your email" name="email"/>
-                            <textArea placeholder="Enter your message" name="message"></textArea>
-                            <button type="submit">Send</button>
+                            <input type="hidden" name="form-name" value="contact" />
+                            <label>Name</label>
+                            <br />
+                            <input type="text" placeholder="Enter your name" name="name" className="px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline focus:ring-2 focus:ring-green-700 focus:border-transparent" />
+                            <br />
+                            <br />
+                            <label>Email</label>
+                            <br />
+                            <input type="email" placeholder="Enter your email" name="email" className="px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline focus:ring-2 focus:ring-green-700 focus:border-transparent" />
+                            <br />
+                            <br />
+                            <label>Send me a message! 😄 </label>
+                            <br />
+                            <textArea placeholder="Enter your message" name="message" className="block w-4/5 p-1 mt-1 text-black border rounded shadow form-textarea focus:outline-none focus:ring-2 focus:ring-green-700 focus:border-transparent"></textArea>
+                            <br />
+                            <button type="submit" className="px-4 py-2 font-semibold text-green-700 bg-transparent border border-green-700 rounded hover:bg-green-700 hover:text-white hover:border-transparent">Send</button>
                         </form>
                     </div>
                 </div>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -45,7 +45,6 @@ export default function HomePage() {
                     </Link>
                 </div>
             </div>
-            <Contact/>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 280"><path fill="#273036" fillOpacity="1" d="M0,64L48,90.7C96,117,192,171,288,192C384,213,480,203,576,181.3C672,160,768,128,864,117.3C960,107,1056,117,1152,122.7C1248,128,1344,128,1392,128L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
             <section className="grey-bg">
                 <BlogPosts textColor="white" />
