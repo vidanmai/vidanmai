b52d05eef4e9da9634520fc36ddfe2b5e7b9f53c
vidanmai <vidanmai@gmail.com>
2021-10-23 22:19:20 +0200

    added new form to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,24 +10,12 @@ export default function Contact() {
                         <h1 className="text-4xl font-bold text-center">Contact</h1>
                     </div>
                     <div className="contactInput">
-                        <form name="contact" method="POST" data-netlify="true">
+                        <form name="contact" method="post" data-netlify="true">
                             <input type="hidden" name="form-name" value="contact"/>
-                            <label htmlFor="username">Name:</label>
-                            <br />
-                            <input type="text" name="name" placeholder="Enter name" className="p-1 text-black border-2 border-black focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent" />
-                            <br />
-                            <label htmlFor="username">Email:</label>
-                            <br />
-                            <input type="text" name="email" placeholder="Enter email" className="p-1 text-black border-2 border-black focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent" />
-                            <label className="block">
-                                <span className="text-black">Message</span>
-                                <textarea name="message" className="block w-4/5 p-1 mt-1 text-black border-2 border-black form-textarea focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
-                                    rows="4" placeholder="Enter message."></textarea>
-                            </label>
-                            <br/>
-                            <p>
-                                <button className="px-4 py-2 font-bold text-white bg-gray-800 rounded hover:bg-blue-700" type="submit">Send</button>
-                            </p>
+                            <input type="text" placeholder="Enter your name" name="name"/>
+                            <input type="email" placeholder="Enter your email" name="email"/>
+                            <textArea placeholder="Enter your message" name="message"></textArea>
+                            <button type="submit">Send</button>
                         </form>
                     </div>
                 </div>
