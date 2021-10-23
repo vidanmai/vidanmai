e11fce5d2477130e699f274aa2aeca4137f93703
vidanmai <vidanmai@gmail.com>
2021-10-23 21:49:34 +0200

    added contact netlify form submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,24 +4,30 @@ import './style/contact.css';
 export default function Contact() {
     return (
         <div className="case">
-            <div className="contactContent py-10">
-                <div className="contactGrid py-20 bg-green-300">
+            <div className="py-10 contactContent">
+                <div className="py-20 bg-green-300 contactGrid">
                     <div className="contactInfo">
                         <h1 className="text-4xl font-bold text-center">Contact</h1>
                     </div>
                     <div className="contactInput">
-                        <label htmlFor="username">Name:</label>
-                        <br />
-                        <input type="text" placeholder="Enter name" className="p-1 text-black border-2 border-black focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent" />
-                        <br />
-                        <label htmlFor="username">Email:</label>
-                        <br />
-                        <input type="text" placeholder="Enter email" className="p-1 text-black border-2 border-black focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent" />
-                        <label className="block">
-                            <span className="text-black">Message</span>
-                            <textarea className="form-textarea mt-1 block w-4/5 p-1 text-black border-2 border-black focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent" 
-                            rows="4" placeholder="Enter message."></textarea>
-                        </label>
+                        <form name="contact" method="POST" data-netlify="true">
+                            <label htmlFor="username">Name:</label>
+                            <br />
+                            <input type="text" placeholder="Enter name" className="p-1 text-black border-2 border-black focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent" />
+                            <br />
+                            <label htmlFor="username">Email:</label>
+                            <br />
+                            <input type="text" placeholder="Enter email" className="p-1 text-black border-2 border-black focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent" />
+                            <label className="block">
+                                <span className="text-black">Message</span>
+                                <textarea className="block w-4/5 p-1 mt-1 text-black border-2 border-black form-textarea focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
+                                    rows="4" placeholder="Enter message."></textarea>
+                            </label>
+                            <br/>
+                            <p>
+                                <button className="px-4 py-2 font-bold text-white bg-gray-800 rounded hover:bg-blue-700" type="submit">Send</button>
+                            </p>
+                        </form>
                     </div>
                 </div>
 
