33cbcdc5c19fd4e5b14ec929e29d437065d9dc1c
vidanmai <vidanmai@gmail.com>
2021-12-01 14:20:13 +0100

    small adjustments to font in nav and blog

--- src/components/Post.js
+++ src/components/Post.js
@@ -48,18 +48,18 @@ export default function Post() {
     //<img src={urlFor(postData.authorImage).width(100).url()} alt="" />
     //<p>Published {new Date(postData.publishedAt).toLocaleString().substring(0, 16)}</p>
     return (
-        <div className="postCase my-2 lg:p-2">
-            <div className="postBody mx-auto 2xl:w-2/5 xl:w-3/5 lg:w-3/5 md:w-4/5 md:p-5">
+        <div className="my-2 postCase lg:p-2">
+            <div className="mx-auto postBody 2xl:w-2/5 xl:w-3/5 lg:w-3/5 md:w-4/5 md:p-5">
                 <div className="postHead">
-                    <h2 className="text-5xl p-1 text-center tracking-wide uppercase font-semibold mb-10">{postData.title}</h2>
-                    <div className="my-2 text-center text-lg">
+                    <h2 className="p-1 mb-8 text-5xl font-semibold tracking-wide text-center uppercase">{postData.title}</h2>
+                    <div className="my-2 text-lg text-center" id="authorText">
                         <p>Written by {postData.name}
                         </p>
                     <p>Published {new Date(postData.publishedAt).toString().substring(4, 10)}</p>
                     </div>
                 </div>
                 <img className="postImg" src={urlFor(postData.mainImage).url()} alt="" />
-                <div className="body text antialiased subpixel-antialiased text-xl" id="postBody">
+                <div className="text-xl antialiased subpixel-antialiased body text" id="postBody">
                     <BlockContent
                         blocks={postData.body}
                         projectId={sanityClient.clientConfig.projectId}
